7ea2dc070df65d0c0d9550fd936999a28983a58e
damien <damien.barrere@gmail.com>
2022-04-20 18:20:58 +0200

    Updates

---
+++ precache-manifest.ebb0a54a8960100881678bdc52fc9ad1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6bc0e602f89e7a70dcd7b384ce979b",
+    "url": "/Jiwoks/Developpeur-Ethereum-Template/index.html"
+  },
+  {
+    "revision": "927f00f5485b0391c887",
+    "url": "/Jiwoks/Developpeur-Ethereum-Template/static/css/main.7441a32e.chunk.css"
+  },
+  {
+    "revision": "d18353c5dbca95542b6e",
+    "url": "/Jiwoks/Developpeur-Ethereum-Template/static/js/2.7e2f5509.chunk.js"
+  },
+  {
+    "revision": "927f00f5485b0391c887",
+    "url": "/Jiwoks/Developpeur-Ethereum-Template/static/js/main.067338e5.chunk.js"
+  },
+  {
+    "revision": "ffcc94ab512772183640",
+    "url": "/Jiwoks/Developpeur-Ethereum-Template/static/js/runtime-main.3ae60fc9.js"
+  }
+]);
